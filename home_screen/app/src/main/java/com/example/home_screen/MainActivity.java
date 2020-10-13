263a04d1aa2ee2d2e60aab968a822912e5be806d
pascuale2 <epas97@hotmail.com>
2020-10-12 21:22:08 -0600

    Initial commit of the home screen UI along with its icons

---
+++ home_screen/app/src/main/java/com/example/home_screen/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.home_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Window window = MainActivity.this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.colorBG_gradientStart));
+    }
+}
