e8ac0d9291afcd5fb85cb999679066ed8bfa88c2
Strembitsky <>
2020-10-27 17:06:29 -0600

    Added partial functionality for an Employee Registration

--- shiftSchedule/app/src/main/java/com/example/shiftschedule/employeePage.java
+++ shiftSchedule/app/src/main/java/com/example/shiftschedule/employeePage.java
@@ -2,7 +2,9 @@ package com.example.shiftschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class employeePage extends AppCompatActivity {
@@ -11,6 +13,14 @@ public class employeePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_page);
+    }
+
+    // onClick function for registerButton to take you to register Page/Activity
+    public void goToAddEmployee(View view) {
+        Log.d("openattempt", "before opening");
+        Intent intent = new Intent(employeePage.this, eregisterpage.class);
+        startActivity(intent);
+        Log.d("open attempt2", "after opening");
     }
 
     public void LPgoBack (View view){
